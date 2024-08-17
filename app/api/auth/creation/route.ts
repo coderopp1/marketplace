1c4080b2386c3d370969e7bfa3035bd6e54e7ce8
aru <arukumargupta@gmail.com>
2024-08-17 20:50:35 +0530

    changed route in auth

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -1,39 +1,78 @@
+// import prisma from "@/app/lib/db";
+// import { auth, currentUser } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   try {
+//     const { userId } = auth();
+//     const user = await currentUser();
+  
+
+//     if (!userId || !user) {
+//       return NextResponse.redirect("http://localhost:3000/"); // Redirect to an error page or login page
+//     }
+
+//     let dbUser = await prisma.user.findUnique({
+//       where: {
+//         id: user.id,
+//       },
+//     });
+
+//     if (!dbUser) {
+//       dbUser = await prisma.user.create({
+//         data: {
+//           id: user.id,
+//           firstName: user.firstName ?? "",
+//           lastName: user.lastName ?? "",
+        
+//         },
+//       });
+//     }
+
+//     return NextResponse.redirect("http://localhost:3000/");
+//   } catch (error) {
+   
+//     return NextResponse.redirect("http://localhost:3000/"); // Redirect to an error page or handle the error
+//   }
+// }
+
 import prisma from "@/app/lib/db";
 import { auth, currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
-  try {
-    const { userId } = auth();
-    const user = await currentUser();
-  
+  noStore();
+  const { userId } = auth();
+  const user = await currentUser();
 
-    if (!userId || !user) {
-      return NextResponse.redirect("http://localhost:3000/"); // Redirect to an error page or login page
-    }
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong...");
+  }
 
-    let dbUser = await prisma.user.findUnique({
-      where: {
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    dbUser = await prisma.user.create({
+      data: {
         id: user.id,
+        firstName: user.firstName ?? "",
+          lastName: user.lastName ?? "",
+        email: user.emailAddresses[0]?.emailAddress ?? "",
+  
+        profileImage:
+        user.imageUrl ?? `https://avatar.vercel.sh/${user.firstName}`,
       },
     });
-
-    if (!dbUser) {
-      dbUser = await prisma.user.create({
-        data: {
-          id: user.id,
-          firstName: user.firstName ?? "",
-          lastName: user.lastName ?? "",
-          email: user.emailAddresses[0]?.emailAddress ?? "",
-          profileImage:
-            user.imageUrl ?? `https://avatar.vercel.sh/${user.firstName}`,
-        },
-      });
-    }
-
-    return NextResponse.redirect("http://localhost:3000/");
-  } catch (error) {
-   
-    return NextResponse.redirect("http://localhost:3000/"); // Redirect to an error page or handle the error
   }
+
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000/"
+      : "https://shoe-marshal.vercel.app/"
+  );
 }
