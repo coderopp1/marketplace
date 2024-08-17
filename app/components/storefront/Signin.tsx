dc7aa7a7568ff4120d82f5b158fa30f5fad5b477
aru <arukumargupta@gmail.com>
2024-08-17 23:10:17 +0530

    errors

--- app/components/storefront/Signin.tsx
+++ app/components/storefront/Signin.tsx
@@ -1,47 +1,16 @@
-// "use client";
-
-// import { SignedOut, SignIn, SignInButton } from "@clerk/nextjs";
-// import { useRouter } from "next/navigation";
-// import { useEffect } from "react";
-
-// const Signin = () => {
-//   const router = useRouter();
-
-//   useEffect(() => {
-//     // Redirect to the specified route after the user signs in
-//     router.push("/api/auth/creation"); // Ensure the path is correct
-//   }, [router]);
-
-//   return (
-//     <SignedOut>
-//       <div className="ml-4">
-//         <SignInButton />
-//       </div>
-//       <SignIn routing="path" path="/api/auth/sign-in" />
-//     </SignedOut>
-//   );
-// };
-
-// export default Signin;
 "use client";
 
-import { SignedOut, SignIn, SignInButton, useUser } from "@clerk/nextjs";
+import { SignedOut, SignIn, SignInButton } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 
 const Signin = () => {
   const router = useRouter();
-  const { isSignedIn } = useUser(); // Check the user's sign-in status
-  const isSignedInRef = useRef(isSignedIn);
 
   useEffect(() => {
-    // Use the ref to store the sign-in status
-    if (isSignedInRef.current) {
-      router.push("/api/auth/creation"); // Redirect to the specified route after the user signs in
-    }
-    // Update the ref with the latest isSignedIn value
-    isSignedInRef.current = isSignedIn;
-  }, [isSignedIn, router]);
+    // Redirect to the specified route after the user signs in
+    router.push("/api/auth/creation"); // Ensure the path is correct
+  }, [router]);
 
   return (
     <SignedOut>
@@ -54,3 +23,4 @@ const Signin = () => {
 };
 
 export default Signin;
+
